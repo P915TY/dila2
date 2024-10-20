5628ed975d3739355a77683e79991288030dde27
Iz_me praveen <Praveensewmina726@gmail.com>
2024-10-20 19:13:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "iRMxRL6a#uxvHlbZ3KQCFApavZfejYF0eb4zzFdHh3nfMRUQ1yiI",
+SESSION_ID: process.env.SESSION_ID || "dzUSQboI#Dbm3GKemFrjT4QVDt46H76e4zHrwlt_HwjGRlQ3mQXw",
 MONGODB: process.env.MONGODB || "mongodb://mongo:wjZXtPKVoyCuzlUVtNWNCKjxpTPAHcTf@autorack.proxy.rlwy.net:36246",
 };
 
